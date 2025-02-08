06be8ce0d111e446c1d75c569ebe8b6fbc2470f1
thiagodivclass <thiago.vidanova@hotmail.com>
2025-02-08 16:17:44 -0300

    prat

--- Aula1/script.js
+++ Aula1/script.js
@@ -2,6 +2,16 @@
 
 // window.confirm('Está indo na força do ódio?')
 
-var disse = window.prompt('Diga aí')
-window.alert('Boa, então é isso!, ' + disse + '!')
+// var disse = window.prompt('Diga aí') 
+// window.alert('Boa, então é isso!, ' + disse + '!')
 
+
+// var n1 = window.prompt('Digite um número')
+// var n2  = Number(window.prompt('Digite outro número')) // Uma forma
+// var s = Number(n1) + n2 // Outra forma
+// window.alert ('A soma dos números digitados é: ' + s)
+// window.alert (`A soma entre ${n1} e ${n2} é igual a ${s}`) //Entre crases
+
+var nome = window.prompt('Digite um texto')
+
+document.write(`O texto tem ${nome.length} letras.` )

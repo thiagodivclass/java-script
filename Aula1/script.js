ebcaec4676a610353547929fed05686801e5ca02
thiagodivclass <thiago.vidanova@hotmail.com>
2025-02-08 14:06:21 -0300

    praticando var

--- Aula1/script.js
+++ Aula1/script.js
@@ -1,6 +1,7 @@
-window.alert('Olá, mundo!.. Hello word!')
+// window.alert('Olá, mundo!.. Hello word!')
 
-window.confirm('Está indo na força do ódio?')
+// window.confirm('Está indo na força do ódio?')
 
-window.prompt('Responda aqui')
+var disse = window.prompt('Diga aí')
+window.alert('Boa, então é isso!, ' + disse + '!')
 

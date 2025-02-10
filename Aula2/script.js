a9ad4f990d759c29cc1970e196f8a0e45d2d99c7
thiagodivclass <thiago.vidanova@hotmail.com>
2025-02-10 15:03:47 -0300

    praticando functions

--- Aula2/script.js
+++ Aula2/script.js
@@ -1,3 +1,7 @@
+var calc = window.document.querySelector('#button')
+calc.addEventListener('click', somar)
+
+
 function somar() {
     var op1 = window.document.getElementById('num')
     var op2 = window.document.querySelector('#num2')
@@ -6,7 +10,13 @@ function somar() {
     var dig2 = Number(op2.value)
     var s = dig1 + dig2
 
-    res.innerHTML= `A soma entre ${dig1} e ${dig2} é igual a ${s}`
+    res.innerHTML= `A soma entre ${dig1} e ${dig2} é igual a ${s.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})}`
 
+}
 
+var clean = document.querySelector('#limp')
+clean.addEventListener('click', apag)
+
+function apag() {
+   res.innerHTML=''
 }

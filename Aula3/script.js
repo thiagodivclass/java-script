1ebde4458bf8b59bad71a1a8ad69096a4dbcdaee
thiagodivclass <thiago.vidanova@hotmail.com>
2025-02-10 21:07:58 -0300

    pratica if else

--- Aula3/script.js
+++ Aula3/script.js
@@ -1,1 +1,27 @@
-console.log('Olá, mundo')
+// var vel = 52
+// if (vel >= 50) {
+//     console.log(`Você pasou a ${vel}km/h, ultrapassou o limite de velocidade!`)
+// }
+
+
+// else {
+//     console.log(`Você passou a ${vel}km/h, muito bom, dirija sempre com segurança!`)
+// }
+ 
+
+
+function verific() {
+    var txt1 = window.document.getElementById('pais').value
+    var state = window.document.getElementById('nacional')
+
+    if (txt1 == 'Brasil') {
+        state.innerText = `Você é brasileiro!`
+    } 
+    else {  state.innerText = `Você é estrangeiro!`}
+
+  
+
+
+
+
+}

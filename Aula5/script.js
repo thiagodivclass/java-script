5bad3987193b6a88948a38503a46718840d0e7ec
thiagodivclass <thiago.vidanova@hotmail.com>
2025-02-14 11:28:16 -0300

    model

--- Aula5/script.js
+++ Aula5/script.js
@@ -1,13 +1,17 @@
-var t = 1
+// var t = 1
 // while (t <= 5){
 //     console.log('aqui tem 5 linhas')
-//     t++
+//     t++ 
 // }
 
-do {
-    console.log('Aqui tem 5 linhas')
-    t++
-}
- while (t <= 5)
+// do {
+//     console.log('Aqui tem 5 linhas')
+//     t++
+// }
+//  while (t <= 5)
 
-    
+console.log('Começou')
+for ( var t = 1; t <= 5; t++) {
+    console.log(t)
+}
+console.log('Acabou')

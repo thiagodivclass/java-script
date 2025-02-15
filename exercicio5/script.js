2c43e697702c4fc58826a35c170c337c2117199d
thiagodivclass <thiago.vidanova@hotmail.com>
2025-02-15 13:16:31 -0300

    media q

--- exercicio5/script.js
+++ exercicio5/script.js
@@ -2,45 +2,71 @@ var campo = document.querySelector('#num')
 var verific = document.querySelector('#confirma')
 var clean = document.querySelector('#limp')
 var mostrar = document.querySelector('#tabuada')
+var telamob = document.querySelector('#texto')
+
 clean.addEventListener('click', apaga)
 verific.addEventListener('click', clique)
 
 
 
 
-function clique(){
+
+function clique() {
 
 
-    
+
     if (campo.value.length <= 0) {
         alert('Erro: Campo vazio, digite um número e gere a tabuada!')
 
     }
-    else {
-    var num = Number(campo.value)
-    if (num < 0){
-       
-        alert('Impossível calcular menor que 0')
-    }
     
-        else{
+    else {
+        var num = Number(campo.value)
+        if (num < 0) {
+
+            alert('Impossível calcular menor que 0')
+        }
+
+        else {
             let a = num
             let b = 1
-            mostrar.innerHTML=''
-            while (b<=10){
-                let item = document.createElement('option')
-                item.text=`${a} x ${b} = ${a*b}`
-                mostrar.appendChild(item)
-                b++
-
-            }
+            telamob.innerHTML = ''
+            mostrar.innerHTML = ''
             
+            while (b <= 10) {
+
+                let item = document.createElement('option')
+                item.text = `${a} x ${b} = ${a * b}`
+                mostrar.appendChild(item)
+                telamob.innerHTML += `${a} x ${b} = ${a * b} <br>`
+                b++
+                
+                
+            }
+
+           
+
+
+
+
         }
-               
+
+
+
+
+
        
     }
+
+
+
+
+
 }
 
-function apaga(){
-     mostrar.innerHTML=''
+
+
+function apaga() {
+    mostrar.innerHTML = ''
+    telamob.innerHTML = ''
 }

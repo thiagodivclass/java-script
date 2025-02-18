413ff4abc0990f84578ff1794cafff4fa67c3d9a
thiagodivclass <thiago.vidanova@hotmail.com>
2025-02-18 20:41:01 -0300

    coments

--- exercicio_array/script.js
+++ exercicio_array/script.js
@@ -1,12 +1,14 @@
-let adiciona = document.querySelector('#add')
+let adiciona = document.querySelector('input#add')
 adiciona.addEventListener('click', incluir)
 let num1 = document.querySelector('#numero')
 let list = document.querySelector('#valores')
 let resposta = document.querySelector('#texto')
+let reload = document.querySelector('#atualizar')
+reload.addEventListener('click', atualize)
 
-let valores = []
+let valores = [] //Vetor
 
-function isNum(n){
+function isNum(n){ // Verificar se é um número
     if (Number(n) >=1 && Number(n)<=100){
         return true
     }
@@ -15,8 +17,8 @@ function isNum(n){
     }
 }
 
-function inList(n, l){
-    if(l.indexOf(Number(n)) !=-1){
+function inList(n, l){ // Recebe o número e uma lista
+    if(l.indexOf(Number(n)) !=-1){ // Se o número for diferente de -1
         return true
     }
     else{
@@ -26,11 +28,11 @@ function inList(n, l){
 
 
 function incluir(){
-  if(isNum(num1.value) && !inList(num1.value, valores)){
-       valores.push(Number(num1.value))
-       let itens = document.createElement('option')
-       itens.text = `Valor ${num1.value} adicionado.`
-       list.appendChild(itens)
+  if(isNum(num1.value) && !inList(num1.value, valores)){ // Incluir se for número e não estiver na lista
+       valores.push(Number(num1.value)) // Adiciona valores no vetor
+       let itens = document.createElement('option') // Cria as optinons
+       itens.text = `Valor ${num1.value} adicionado.` // Adiciona os valores nas options
+       list.appendChild(itens) // Adiciona as options no select 
        resposta.innerHTML=''
 
   }
@@ -46,24 +48,24 @@ let finish = document.querySelector('#fim')
 finish.addEventListener('click', finalizar)
 
 function finalizar(){
-    if (valores.length == 0){
+    if (valores.length == 0){ // Impede valores vazios 
         alert('Adicione valores para finalizar!')
     }
     else {
-        let total = valores.length
-        let maior =valores[0]
-        let menor =valores[0]
-        let soma = 0
-        let media = 0
+        let total = valores.length // Quantidade de dados no vetor 
+        let maior =valores[0] // Maior número baseado no ínicio do cálculo dentro do vetor
+        let menor =valores[0] // Menor número baseado no ínicio do cálculo dentro do vetor
+        let soma = 0 // Valor inicial da soma
+        let media = 0 // Valor inicial da média
 
-        for(let pos in valores){
-            soma+=valores[pos]
+        for(let pos in valores){ // Para cada posição no vetor (Valores)
+            soma+=valores[pos] 
             media = soma/total
-            if (valores[pos] > maior)
-                maior = valores[pos]
+            if (valores[pos] > maior) // Se a posição do valor no vetor for maior que a var "maior"
+                maior = valores[pos] // Var maior recebe o valor da posição no vetor
             
-            if(valores[pos]< menor)
-                menor = valores[pos]
+            if(valores[pos]< menor) // Se a posição do valor no vetor for menor que a var "menor"
+                menor = valores[pos] // Var menor recebe o valor da posição no vetor
         }
         resposta.innerHTML = '' 
         resposta.innerHTML +=`<p>Ao todo, temos ${total} números cadastrados.</p>`
@@ -73,4 +75,8 @@ function finalizar(){
         resposta.innerHTML +=`<p>A média dos valores adicionados é ${media}.</p>`
        
     }
+}
+
+function atualize(){
+    document.location.reload()
 }
